7dcbcf078a8ffbe6b1ee2ae3cf657c520a40a9e1
T.Beier <tim_beier@hotmail.de>
2016-09-28 10:55:18 +0200

    slime collider now get disabled when they get killed terrain does now wobble everytime the player walks over it

--- tictactoe/Assets/Assets/scripts/TriggerAnimation.js
+++ tictactoe/Assets/Assets/scripts/TriggerAnimation.js
@@ -5,10 +5,17 @@ function OnTriggerEnter2D(Other : Collider2D){
     if(Other.gameObject.name == "Player"){
 
         animator.SetTrigger("OnHit");
-        //not resetting yet 
+
+        if(this.gameObject.tag == "Ground") {
+            yield WaitForSeconds (5);
+            animator.ResetTrigger("OnHit");
+        }
 
         if(this.gameObject.tag == "Monster") {
+            yield WaitForSeconds (0.5);
+            transform.parent.gameObject.GetComponent(BoxCollider2D).enabled=false;
             Destroy (transform.parent.gameObject, 1);
         }
     }
+
 }
