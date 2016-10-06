db137c8427b51b227b08629555d7496058caeeb3
T.Beier <tim_beier@hotmail.de>
2016-10-06 21:27:47 +0200

    Removed all platform effector 2d as they do not result in less wall friction. Added destructable box prefab with DestroyOnHit script.

---
+++ tictactoe/Assets/scripts/DestroyOnHit.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+var child: Transform;
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter2D(Other : Collider2D){
+    if(Other.gameObject.tag == "Player" || Other.gameObject.tag == "GlobalDmg"){
+
+        child.GetComponent(Animator).SetTrigger("OnHit");
+        gameObject.tag = "Dead";
+        yield WaitForSeconds (0.1);
+        //gameObject.GetComponent(BoxCollider2D).enabled=false;
+        Destroy (gameObject, 0.5);
+    }
+}
